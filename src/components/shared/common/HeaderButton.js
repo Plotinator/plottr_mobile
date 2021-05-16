09afe29476868054f9cf0f96c73fe86b17d6be38
Arian Hendricks <arian.hendricks@gmail.com>
2021-05-16 14:31:42 -0500

    HeaderButton fix (filter button display fix)

--- src/components/shared/common/HeaderButton.js
+++ src/components/shared/common/HeaderButton.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View } from 'react-native'
 import Text from './Text'
 import ShellButton from './ShellButton'
@@ -6,12 +6,14 @@ import { t } from 'plottr_locales'
 import { Icon } from 'native-base'
 import styles from './HeaderButtonStyles'
 
-export default (props) => {
-  const { style, title = t('Categories'), icon = 'list', ...otherProps } = props
-  return (
-    <ShellButton {...otherProps} style={[styles.container, style]}>
-      <Icon type='FontAwesome5' name={icon} style={styles.icon} />
-      <Text style={styles.text}>{title}</Text>
-    </ShellButton>
-  )
+export default class HeaderButton extends Component {
+  render () {
+    const { style, title = t('Categories'), icon = 'list', ...otherProps } = this.props
+    return (
+      <ShellButton {...otherProps} style={[styles.container, style]}>
+        <Icon type='FontAwesome5' name={icon} style={styles.icon} />
+        <Text style={styles.text}>{title}</Text>
+      </ShellButton>
+    )
+  }
 }
