9bac5b70614f2cca6cb7133c0851c8a105686466
Arian Hendricks <arian.hendricks@gmail.com>
2021-07-18 17:04:31 -0500

    Clean up to removed references

--- src/components/phone/navigators/RootPhoneNavigator.js
+++ src/components/phone/navigators/RootPhoneNavigator.js
@@ -1,20 +1,13 @@
 import React, { useRef, useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Drawer } from 'native-base'
-import { t } from 'plottr_locales'
 import MainTabs from './MainTabs'
-import CustomAttributesScreen from '../../shared/customAttributes/CustomAttributesScreen'
-import { attachmentHeaderTitles } from '../../../utils/attachment_titles'
 import SideBar from '../../shared/SideBar'
-import PlotlinesStack from './PlotlinesStack'
-import AttachmentSelectorModal from '../shared/AttachmentSelectorModal'
-import ColorPickerScreen from '../shared/ColorPickerScreen'
 import ErrorBoundary from '../../shared/ErrorBoundary'
-import { RenderTitle } from '../../shared/common'
 
 const RootStack = createStackNavigator()
 
-export default function RootPhoneNavigator (props) {
+export default function RootPhoneNavigator(props) {
   const drawerRef = useRef(null)
   const closeDrawer = () => drawerRef.current._root.close()
   const openDrawer = () => drawerRef.current._root.open()
@@ -37,35 +30,6 @@ export default function RootPhoneNavigator (props) {
             component={MainTabs}
             options={{ headerShown: false }}
             initialParams={{ openDrawer }}
-          />
-          <RootStack.Screen
-            name='CustomAttributesModal'
-            component={CustomAttributesScreen}
-            options={{
-              title: RenderTitle('Custom Attributes'),
-              headerBackTitle: RenderTitle('Done')
-            }}
-          />
-          <RootStack.Screen
-            name='AttachmentSelectorModal'
-            component={AttachmentSelectorModal}
-            options={({ route }) => ({
-              title: attachmentHeaderTitles(route.params.type),
-              headerBackTitle: RenderTitle('Done')
-            })}
-          />
-          <RootStack.Screen
-            name='PlotlinesModal'
-            component={PlotlinesStack}
-            options={{ headerShown: false }}
-          />
-          <RootStack.Screen
-            name='ColorPickerModal'
-            component={ColorPickerScreen}
-            options={{
-              title: RenderTitle('Color Picker'),
-              headerBackTitle: RenderTitle('Back')
-            }}
           />
         </RootStack.Navigator>
       </Drawer>
--- src/components/tablet/navigators/RootTabletNavigator.js
+++ src/components/tablet/navigators/RootTabletNavigator.js
@@ -3,12 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack'
 import MainTabs from './MainTabs'
 import SideBar from '../../shared/SideBar'
 import { Drawer } from 'native-base'
-import CustomAttributesScreen from '../../shared/customAttributes/CustomAttributesScreen'
-import { RenderTitle } from '../../shared/common'
 
 const RootStack = createStackNavigator()
 
-export default function RootTabletNavigator (props) {
+export default function RootTabletNavigator(props) {
   const drawerRef = useRef(null)
   const closeDrawer = () => drawerRef.current._root.close()
   const openDrawer = () => drawerRef.current._root.open()
@@ -31,14 +29,6 @@ export default function RootTabletNavigator (props) {
           component={MainTabs}
           options={{ headerShown: false }}
           initialParams={{ openDrawer }}
-        />
-        <RootStack.Screen
-          name='CustomAttributesModal'
-          component={CustomAttributesScreen}
-          options={{
-            title: RenderTitle('Custom Attributes'),
-            headerBackTitle: RenderTitle('Done')
-          }}
         />
       </RootStack.Navigator>
     </Drawer>
